899fa177e43deb9b0672b47fe9d203813372ff81
otienojoseph <otienoj.cruz@gmail.com>
2023-06-28 11:05:56 +0000

    Add character specifiers

--- _printf.c
+++ _printf.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
 
 /**
   * _printf - Produce output according to a format
@@ -9,5 +12,34 @@
 
 int _printf(const char *format, ...)
 {
-	return (0);
+	int c_count;
+	va_list args;
+	char c_value;
+	char *s_value;
+	int i;
+
+	c_count = strlen(format);
+	/* Make args point to the first unnamed value */
+	va_start(args, format);
+
+	for (i = 0; i < c_count; i++)
+	{
+		if (format[i] == 'c')
+		{
+			c_value = (char) va_arg(args, int);
+			printf("%c\n", c_value);
+		}
+		else if (format[i] == 's')
+		{
+			s_value = va_arg(args, char*);
+			printf("%s\n", s_value);
+		}
+		else if (format[i] == '%')
+		{
+			printf("%s\n", format);
+		}
+	}
+
+	va_end(args);
+	return (c_count);
 }
