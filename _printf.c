d89ef01986679143a3eba754113398f0e6a6bf8d
otienojoseph <otienoj.cruz@gmail.com>
2023-06-28 22:16:55 +0000

    Add checks for c and s specifiers

--- _printf.c
+++ _printf.c
@@ -9,14 +9,12 @@
   * @...: Rest of the arguments
   * Return: Return number of characters printed
   */
-
 int _printf(const char *format, ...)
 {
-	int c_count;
+	int c_count, i;
 	va_list args;
 	char c_value;
 	char *s_value;
-	int i;
 
 	c_count = strlen(format);
 	/* Make args point to the first unnamed value */
@@ -24,7 +22,11 @@ int _printf(const char *format, ...)
 
 	for (i = 0; i < c_count; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+		}
+		else if (format[i] == '%')
 		{
 			format++;
 
@@ -32,18 +34,15 @@ int _printf(const char *format, ...)
 			{
 				c_value = (char) va_arg(args, int);
 				write(1, &c_value, 1);
-				write(1, "\n", 1);
 			}
 			else if (format[i] == 's')
 			{
 				s_value = va_arg(args, char*);
 				write(1, s_value, strlen(s_value));
-				write(1, "\n", 1);
 			}
 			else if (format[i] == '%')
 			{
-				write(1, format, 1);
-				write(1, "\n", 1);
+				write(1, "", 0);
 			}
 		}
 	}
